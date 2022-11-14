8915b0f05f4db080295822999140488662d2fce4
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-11-14 13:03:21 +0100

    git

---
+++ src/main/java/com/example/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,20 @@
+package com.example.sfgdi.controllers;
+
+import com.example.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(@Qualifier("constructorGreetingServiceImpl") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreetingService() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/com/example/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.sfgdi.controllers;
+
+import com.example.sfgdi.services.ConstructorGreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller=new ConstructorInjectedController(new ConstructorGreetingServiceImpl());
+
+    }
+
+    @Test
+    void getGreetingService() {
+        System.out.println(controller.getGreetingService());
+    }
+}
